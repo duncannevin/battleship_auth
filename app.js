1c3cac3e5af87bb62101ee5424ac1b01bff8dbd5
duncan nevin <duncan@junolive.co>
2021-02-13 08:09:51 -0800

    Oh cors

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const log4js = require('log4js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('./database');
 
-const cors = require('./middleware/cors');
 const passwordHasher = require('./middleware/password-hasher');
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(passwordHasher);
 app.use(log4js.connectLogger(log4js.getLogger('http'), {level: 'auto'}));
